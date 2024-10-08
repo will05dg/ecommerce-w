eb7e4b7ba1898ff8b9908d86f04721cff5c25d5b
Wilson Gaston Villa Camargo <will.05.dg@gmail.com>
2024-10-08 17:03:34 -0300

    navbar

--- src/components/leyour/navbar/Navbar.jsx
+++ src/components/leyour/navbar/Navbar.jsx
@@ -7,9 +7,9 @@ export const Navbar = () => {
             <div className="container-navbar">
                 <img src="https://res.cloudinary.com/dsphjfhea/image/upload/v1728271324/Mesa_de_trabajo_1_ekn2hk.svg" alt="logo de lapiz" className="logo" />
                 <ul>
-                    <li>uno</li>
-                    <li>dos</li>
-                    <li>tres</li>
+                    <li>productos</li>
+                    <li>marcas</li>
+                    <li>tintas</li>
                 </ul>
                 < CartWidget />
             </div>
