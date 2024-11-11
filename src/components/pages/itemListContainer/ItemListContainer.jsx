279a98133fc99edf3e205563f8a980377d20913a
Wilson Gaston Villa Camargo <will.05.dg@gmail.com>
2024-11-10 22:40:52 -0300

    loan

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -3,6 +3,7 @@ import { productosArte } from "../../../productos";
 import { useEffect, useState } from "react";
 import { ItemList } from "./ItemList";
 import { useParams } from "react-router-dom";
+import { Skeleton } from "@mui/material";
 
 export const ItemListContainer = () => {
 
@@ -30,7 +31,17 @@ export const ItemListContainer = () => {
     }, [categoria]);
 
     if (myProductosArte.length == 0) {
-        return <h2>cargando ...</h2>
+        return (
+            <div className="grilla">
+                <div className="cartasloadin">
+                    <Skeleton variant="rectangular" width={221} height={200} className="imagenloandin" />
+                    <Skeleton variant="text" sx={{ fontSize: '3rem' }} width={210} />
+                    <Skeleton variant="text" sx={{ fontSize: '2rem' }} width={200} />
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={160} />
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} width={160} />
+                    <Skeleton variant="text" sx={{ fontSize: '2rem' }} width={150} className="botonlondin" />
+                </div>
+            </div>)
     }
 
     return (
