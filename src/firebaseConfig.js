a6c34e232c71cc24f0ef2d2b631f3e8d5de8500c
Wilson Gaston Villa Camargo <will.05.dg@gmail.com>
2024-11-14 23:56:45 -0300

    variables protegidas

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZYJ97NEDhCTjVQ9SpzU9oIZ9tdnHv81U",
-  authDomain: "backend-ecommerce-ca04a.firebaseapp.com",
-  projectId: "backend-ecommerce-ca04a",
-  storageBucket: "backend-ecommerce-ca04a.firebasestorage.app",
-  messagingSenderId: "73318482664",
-  appId: "1:73318482664:web:4aede3e376457f13f4bf5d",
+  apiKey: import.meta.env.VITE_API_KAY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APPI_ID,
 };
 
 const app = initializeApp(firebaseConfig);
